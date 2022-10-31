b056dd4599e24449739bbac90ee9733b4fd4536f
okabe <irvaniali79@gmail.com>
2022-10-31 19:13:10 +0330

    shuffle mode

--- Adaline network/main.py
+++ Adaline network/main.py
@@ -1,6 +1,6 @@
 from utils.Cilib import *
 import matplotlib.pyplot as plt
-
+import numpy as np
 inputdata = [
     file_into_numpy("./coordinators/1.txt"),
     file_into_numpy("./coordinators/2.txt"),
@@ -11,7 +11,7 @@ inputdata = [
 ]
 for index in range(len(inputdata)):
     net_input = inputdata[index]
-
+    np.random.shuffle(net_input)
     x_train, y_train = x_y_split(net_input)
 
     adaline = Adaline()
